183b047d4551bfc3a9787c298e1843f5b1132531
McRae <76176302+McRaeSoftware@users.noreply.github.com>
2022-02-07 12:31:17 +0000

    Created Db Context

---
+++ Data/JustWatchDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using JustWatch.Models;
+    
+namespace JustWatch.Data
+{
+    public class JustWatchDBContext : DbContext
+    {
+        public JustWatchDBContext(DbContextOptions<JustWatchDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users{ get; set; }
+    }
+}
